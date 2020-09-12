44d9062d0d499e00e6c5d5fad7e941c31871ade4
Eyal Golan <eyalgolan96@gmail.com>
2020-09-12 17:56:06 +0300

    add documentation and break down into functions

--- src/server/server.js
+++ src/server/server.js
@@ -1,7 +1,5 @@
-import User from "./src/models/user";
 import mailgun from "mailgun-js";
 import { generateDbConnectionFailedEmail } from "./src/resources/emails";
-import path from "path";
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
@@ -10,11 +8,22 @@ import routers from "./src/routes/";
 dotenv.config();
 import logger from './logger';
 
+// app configuration
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Mongo setup
+// mail configuration
+const mg = mailgun({
+  apiKey: process.env.MAILGUN_APIKEY,
+  domain: process.env.MAILGUN_DOMAIN,
+});
+
+
+/*
+* Mongo setup
+*/
+const connetToMongo = () => {
 const uri = process.env.ATLAS_URI;
 try {
   mongoose.connect(
@@ -25,11 +34,8 @@ try {
 } catch (err) {
   logger.error("DB connection error: " + err);
   try {
+    // send email about db connection error
     logger.info("Trying to send email to admin about error...");
-    const mg = mailgun({
-      apiKey: process.env.MAILGUN_APIKEY,
-      domain: process.env.MAILGUN_DOMAIN,
-    });
     const data = generateDbConnectionFailedEmail(err);
     mg.messages().send(data, function (error, body) {
       logger.info(body);
@@ -38,20 +44,30 @@ try {
     logger.error("Sending email failed with error: " + err);
   }
 }
+};
 
-// User page routing
+/*
+* Adding routings tp the relevant pages
+*/
+const addRoutes = () => {
+  // User page routing
+  app.use("/users", routers.userRouter);
+  // Index page routing
+  app.use("/index", routers.indexRouter);
+  // Publications page routing
+  app.use("/publications", routers.publicationsRouter);
+}
 
-app.use("/users", routers.userRouter);
+/*
+* Starts the server
+*/
+const startServer = () => {
+  const port = process.env.PORT;
+  app.listen(port, () => {
+    logger.info(`Server is running on port: ${port}`);
+  });
+}
 
-// Index page routing
-app.use("/index", routers.indexRouter);
-
-// Publications page routing
-app.use("/publications", routers.publicationsRouter);
-
-const port = process.env.PORT;
-
-// Starts the server
-app.listen(port, () => {
-  logger.info(`Server is running on port: ${port}`);
-});
+connetToMongo();  // Mongo setup
+addRoutes();      // Adds routings to the relevant pages
+startServer();    // Starts the server
