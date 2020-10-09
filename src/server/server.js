d2bbed0de591fb97c64a7b8b77aa2be2f634fc49
ariel_fenster <arielfenster1610@gmail.com>
2020-10-09 12:58:03 +0300

    changed static load path

--- src/server/server.js
+++ src/server/server.js
@@ -60,7 +60,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('src/client/public'));
 
   // Load the static
-  app.get('/', (req, res) => {
+  app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname,'../src','client','public','index.html'));
   });
 }
