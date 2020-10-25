592d82a734d9942dcd7f380a34e4acc053bdbd11
teddy@dnc-algo.com <lazebnik.teddy@gmail.com>
2020-10-25 07:14:54 +0200

    fix email settings

--- src/server/emailController.js
+++ src/server/emailController.js
@@ -4,12 +4,13 @@ import dotenv from "dotenv";
 import { google } from 'googleapis'
 dotenv.config();
 
-const { OAuth2 } = google.auth;
-const oauth2Client = new OAuth2(
-    process.env.CLIENT_ID,
-    process.env.CLIENT_SECRET,
-    process.env.OAUTH,
-  );
+var mailClient = nodemailer.createTransport({
+	service: 'gmail',
+	auth: {
+		user: process.env.EMAIL_USER,
+		pass: process.env.EMAIL_PASSWORD
+	}
+});
 
 /* 
 * email controller configuration - using gmail's api with nodemailer
@@ -17,39 +18,18 @@ const oauth2Client = new OAuth2(
 const Mailing = {};
 
 //Gets email details and sends it
-Mailing.sendEmail = data => {
-    // setting the refresh token
-    oauth2Client.setCredentials({
-    refresh_token: process.env.REFRESH_TOKEN,
-    });
-    // getting the access token
-    const accessToken = oauth2Client.getAccessToken();
+Mailing.sendEmail = data => {	
+	mailClient.sendMail(mailOptions, function(error, info)
+	{
+		if (error) 
+		{
+			logger.error("Sending email failed with error: " + err);
+		} 
+		else 
+		{
+			console.log('Email sent: ' + info.response);
+		}
+	});
+};
 
-    // create the auth object
-    const auth = {
-        type: process.env.SMTP_AUTH_TYPE,
-        user: process.env.EMAIL_USER,
-        clientId: process.env.CLIENT_ID,
-        clientSecret: process.env.CLIENT_SECRET,
-        refreshToken: process.env.REFRESH_TOKEN,
-        accessToken,
-    };
-
-    // create the transporter object
-    const transporter = nodemailer.createTransport({
-        host: process.env.SMTP_HOST,
-        port: process.env.SMTP_PORT,
-        secure: true,
-        service:process.env.SMTP_HOST,
-        auth: auth,
-      });
-    transporter.sendMail(data, function(err, info){
-      if(err) {
-        logger.error("Sending email failed with error: " + err);
-      } else {
-        logger.info('Email sent');
-      }
-    });
-  };
-
-  export default Mailing;
+export default Mailing;
